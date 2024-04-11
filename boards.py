fea13eee952f0197869071c44356b4ab8cfacb9a
Wojciech <wojciech.stuchlak@gmail.com>
2024-04-11 04:38:32 +0200

    feat: added button placement

--- boards.py
+++ boards.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tile import Tile
 
 
 class Board(tk.Frame):
@@ -14,3 +15,14 @@ class Board(tk.Frame):
 
         # row 1 is for board row 0 is for top bar/score bar
         self.grid(row=1, column=0)
+
+        self.buttons = [Tile(self, icons) for i in range(9)]
+
+        self.place_buttons()
+
+    def place_buttons(self):
+        temp = 0
+        for i in range(3):
+            for j in range(3):
+                self.buttons[temp].grid(column=i, row=j, padx=5, pady=5)
+                temp += 1
